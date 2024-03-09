51fabb65e68d5eacbf474ead4e6c2dd6d63c2b61
Tyrn <a@b.com>
2024-03-09 21:29:30 +0300

    Join and split tested

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,7 +9,8 @@ main =
   hspec $ do
     describe "join" $ do
       it "works" $ do
-        T.intercalate "," ["alfa", "bravo"] `shouldBe` "alfa,bravo"
+        T.intercalate "-" ["alfa", "bravo"] `shouldBe` "alfa-bravo"
+        T.splitOn "-" "alfa-bravo" `shouldBe` ["alfa", "bravo"]
     describe "cmpstrNaturally" $ do
       it "works" $ do
         cmpstrNaturally "" "" `shouldBe` EQ
